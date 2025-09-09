b943b0fbe9de9f0456dc0f6d9d5da9da1e782833
listedmile <listedmile@gmail.com>
2025-09-10 00:02:43 +0530

    delete whole account post hook for student

--- frontend/OptiRoll/ApiUrl.jsx
+++ frontend/OptiRoll/ApiUrl.jsx
@@ -1,3 +1,3 @@
-export const ApiUrl = "https://optiroll.onrender.com";
+// export const ApiUrl = "https://optiroll.onrender.com";
 // export const ApiUrl = "http://localhost:3000";
-// export const ApiUrl = "http://192.168.29.110:3000";
+export const ApiUrl = "http://192.168.29.110:3000";
