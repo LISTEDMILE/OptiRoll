3a6ecd60f2caaea64c82a2bee0156fc7fa1ebc46
listedmile <listedmile@gmail.com>
2025-10-02 15:05:07 +0530

    ok

--- frontend/OptiRoll/ApiUrl.jsx
+++ frontend/OptiRoll/ApiUrl.jsx
@@ -1,4 +1,4 @@
 export const ApiUrl = "https://optiroll.onrender.com";
 // export const ApiUrl = "http://localhost:3000";
 // export const ApiUrl = "http://192.168.29.110:3000";
-// export const ApiUrl = "http://192.168.1.101:3000";
+// export const ApiUrl = "http://192.168.1.100:3000";
